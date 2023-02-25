caac5d1f6a29964620fe64a249e18991d23419a3
flaviovidalhuax <flaviovidalhuax@gmail.com>
2023-02-24 20:26:32 -0600

    firt comite eskeleton

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+
+const responseHandlers = require('./utils/handleResponses')
+const db = require('./utils/database')
+const initModels = require('./models/initModels')
+
+const userRouter = require('./users/users.router')
+const authRouter = require('./auth/auth.router')
+const configs=require('../config').api
+const app = express()
+
+app.use(express.json())
+
+db.authenticate()
+    .then(() => console.log('Database authenticated'))
+    .catch(err => console.log(err))
+
+db.sync()
+    .then(() => console.log('Database Synced'))
+    .catch(err => console.log(err))
+
+initModels()
+
+app.get('/', (req, res) => {
+    responseHandlers.success({
+        res,
+        status: 200,
+        message: 'Servidor inicializado correctamente',
+        data: {
+            "users":`${configs.host}/api/v1/users`,
+        }
+    })
+})
+
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/auth', authRouter)
+
+app.use('*', (req, res)=> {
+    responseHandlers.error({
+        res,
+        status: 404,
+        message: `URL not found, please try with ${configs.port}`,
+    })
+})
+
+app.listen(configs.port,() => {
+    console.log('Server started at port 9000')
+})
